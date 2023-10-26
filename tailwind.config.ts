ba59cd6bb1a529d5b742dbb8f0880e39afa3f5ae
lzc256 <36326574+lzc256@users.noreply.github.com>
2023-10-26 10:19:09 +0800

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,7 @@ import type { Config } from 'tailwindcss';
 import colors from 'tailwindcss/colors';
 
 export default {
+  darkMode: 'class',
   content: ['./theme/**/*.ftl'],
   experimental: {
     optimizeUniversalDefaults: true,
